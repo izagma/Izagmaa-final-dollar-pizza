6d33f60bb67bef329f40701a7cb5e21d6d1feaec
Izagma Alonso <ialonso@manhattanbridgeshs.org>
2020-08-19 19:30:12 +0000

    final dollar pizza

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/footer.css';
+
+function Footer() {
+  return(
+    <div className='Footer'>
+      <footer>
+        <p><b>Address:</b> 555 Time Square, NY, NY 10000. <b>Phone</b>: (212) 555-5555</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
